271530e16974c10e450983729ddac9f2c1ccffce
chova <chovavea@outlook.com>
2020-07-19 20:32:32 +0800

    生成ID的工具类

---
+++ siege-core/src/main/java/com/oxford/core/util/IdUtil.java
@@ -0,0 +1,24 @@
+package com.oxford.core.util;
+
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
+
+/**
+ * ID工具类
+ *
+ * @author Chova
+ * @date 2020-07-19
+ */
+public class IdUtil {
+
+    /**
+     * 使用基于时间的生成器生成ID
+     *
+     * @return String类型的UUID
+     */
+    public static String generateUUID() {
+        TimeBasedGenerator timeBasedGenerator = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+        return timeBasedGenerator.generate().toString();
+    }
+}
