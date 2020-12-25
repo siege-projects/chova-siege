f1ad1188181731244eb18624e14435b7c4520210
Chova <1450112292@qq.com>
2020-12-25 13:49:58 +0800

    添加单例模式线程不安全懒汉Singleton示例

---
+++ siege-core/src/main/java/com/oxford/core/design/singleton/UnsafeLazy.java
@@ -0,0 +1,22 @@
+package com.oxford.core.design.singleton;
+
+/**
+ * 单例模式 - 线程不安全懒汉
+ *
+ * @author Chova
+ * @date 2020/12/25
+ */
+public class UnsafeLazy {
+
+    private static UnsafeLazy instance;
+
+    public UnsafeLazy() {
+    }
+
+    public static UnsafeLazy getInstance() {
+        if (null == instance) {
+            instance = new UnsafeLazy();
+        }
+        return instance;
+    }
+}
