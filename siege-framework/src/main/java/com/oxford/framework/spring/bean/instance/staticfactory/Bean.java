304ed044566d1ca3522cb2c80d89dcf208348aef
Chova <1450112292@qq.com>
2021-01-18 08:53:14 +0800

    添加SpringBean静态工厂实例化示例

---
+++ siege-framework/src/main/java/com/oxford/framework/spring/bean/instance/staticfactory/Bean.java
@@ -0,0 +1,23 @@
+package com.oxford.framework.spring.bean.instance.staticfactory;
+
+/**
+ * Bean对象
+ *
+ * @author Chova
+ * @date 2021/01/13
+ */
+public class Bean {
+
+    /**
+     * Bean对象的成员变量
+     */
+    public String variable;
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+}
---
+++ siege-framework/src/main/java/com/oxford/framework/spring/bean/instance/staticfactory/StaticBeanFactory.java
@@ -0,0 +1,19 @@
+package com.oxford.framework.spring.bean.instance.staticfactory;
+
+/**
+ * 静态Bean工厂
+ *
+ * @author Chova
+ * @date 2021/01/13
+ */
+public class StaticBeanFactory {
+
+    /**
+     * 静态Bean工厂类中获取Bean实例的方法
+     *
+     * @return Bean Bean类的Bean实例
+     */
+    public static Bean getBean() {
+        return new Bean();
+    }
+}
