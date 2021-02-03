53e170461f8a663fcf4741d12909a4bd85cf5fee
Chova <1450112292@qq.com>
2021-02-03 15:54:36 +0800

    添加转化日期格式操作

--- siege-core/src/main/java/com/oxford/core/convert/Convert.java
+++ siege-core/src/main/java/com/oxford/core/convert/Convert.java
@@ -1,5 +1,10 @@
 package com.oxford.core.convert;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 数据转换类
  *
@@ -7,6 +12,8 @@ package com.oxford.core.convert;
  * @date 2020/9/30
  */
 public class Convert {
+
+    private static Pattern pattern = Pattern.compile("[\\u4e00-\\u9fa5]+");
 
     /**
      * 将字节数组转化为16进制的字符串
@@ -45,5 +52,31 @@ public class Convert {
             bytes[i] = (byte) (high * 16 + low);
         }
         return bytes;
+    }
+
+    /**
+     * 将形如 xxxx年xx月xx日 xx:xx:xx 格式的时间转换为: xxxx-xx-xx xx:xx:xx 格式的时间
+     *
+     * @param timeStr xxxx年xx月xx日 xx:xx:xx
+     * @return String xxxx-xx-xx xx:xx:xx
+     */
+    public static String toDateFormat(String timeStr) {
+        int index = 0;
+        Matcher matcher = pattern.matcher(timeStr);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            try {
+                if (index == 2) {
+                    matcher.appendReplacement(sb, URLEncoder.encode("", "UTF-8"));
+                    continue;
+                }
+                matcher.appendReplacement(sb, URLEncoder.encode("-", "UTF-8"));
+                index++;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 }
